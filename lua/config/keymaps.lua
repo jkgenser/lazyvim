42699592cf2acd81119fee96654f07877624b45d
jerry genser <jerrygenser@gmail.com>
2024-07-11 10:25:06 -0400

    Add: my keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<Esc>", "<cmd>noh<CR>", { desc = "general clear highlights" })
+vim.keymap.set("n", "<leader>/", "gcc", { desc = "comment toggle", remap = true })
+vim.keymap.set("v", "<leader>/", "gc", { desc = "comment toggle", remap = true })
+vim.g.clipboard = "unnamedplus"
