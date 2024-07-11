d5cd3ffaed20ddd876283539bd1309d95c31c983
jerry genser <jerrygenser@gmail.com>
2024-07-11 15:29:52 -0400

    add: all the remaining plugins

---
+++ lua/plugins/colors.lua
@@ -0,0 +1,12 @@
+return {
+  { "Mofiqul/vscode.nvim" },
+  -- { "navarasu/onedark.nvim" },
+  -- { "ellisonleao/gruvbox.nvim" },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "vscode",
+    },
+  },
+}
