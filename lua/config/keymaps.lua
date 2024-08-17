0745f051b8e5db923839e01d433b6d2193293291
jerry genser <jerrygenser@gmail.com>
2024-08-17 13:35:02 -0400

    updates

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,4 +5,4 @@
 vim.keymap.set("n", "<Esc>", "<cmd>noh<CR>", { desc = "general clear highlights" })
 vim.keymap.set("n", "<leader>/", "gcc", { desc = "comment toggle", remap = true })
 vim.keymap.set("v", "<leader>/", "gc", { desc = "comment toggle", remap = true })
-vim.g.clipboard = "unnamedplus"
+-- vim.g.clipboard = "unnamedplus"
